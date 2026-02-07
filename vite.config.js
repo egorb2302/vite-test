d429422bcc96ffd518d49ecb8b55bb154618d4a0
egorb2302 <zxcgovnoedig@gmail.com>
2026-02-07 15:12:10 +0400

    find fow work with multipage ( vite.config )

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import * as path from 'path';
+
+const pages = {
+    index: resolve(__dirname, "index.html"),
+    index2: resolve(__dirname, "index2.html"),
+}
 
 export default defineConfig({
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
         }
-    }
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                ...pages,
+            },
+        },
+    },
 })
