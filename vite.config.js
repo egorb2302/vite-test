6d837406520bba66dc70edb82cdd295ec9288fb6
egorb2302 <zxcgovnoedig@gmail.com>
2026-02-07 15:00:46 +0400

    add second page with style for it

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import { path } from 'path'
+import { defineConfig } from 'vite';
+import * as path from 'path';
 
 export default defineConfig({
     resolve: {
